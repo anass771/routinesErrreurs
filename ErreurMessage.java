aa60c0693abd4bc33c34854332a09ce485e0fbf7
anass771 <daouanass1@gmail.com>
2021-10-28 16:35:05 +0100

    Create ErreurMessage.java

---
+++ ErreurMessage.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package routines;
+
+/**
+ *
+ * @author anass.daou
+ */
+public class ErreurMessage {
+    private String Message;
+    
+     public ErreurMessage() {
+    }
+
+    public ErreurMessage(String Message) {
+        this.Message = Message;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String Message) {
+        this.Message = Message;
+    }
+
+    
+    
+    
+}
